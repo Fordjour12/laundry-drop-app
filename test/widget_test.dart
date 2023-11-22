adfa299e36535a89a41ab646bb81001785aad2d2
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-22 11:48:46 +0000

    Update import statement in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:laundry_drop_app/main.dart';
+import 'package:in.laundrydrop.app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
