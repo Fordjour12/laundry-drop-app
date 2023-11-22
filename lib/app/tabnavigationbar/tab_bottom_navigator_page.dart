76884ad0ec0e73e8659712e402653c0119a3463c
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-22 16:36:15 +0000

    Refactor tab navigation and add router

--- lib/app/tabnavigationbar/tab_bottom_navigator_page.dart
+++ lib/app/tabnavigationbar/tab_bottom_navigator_page.dart
@@ -1,37 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/src/state.dart';
+import 'package:go_router/go_router.dart';
 import 'package:in.laundrydrop.app/app/tabnavigationbar/tab_bottom_navigator_controller.dart';
 import 'package:provider/provider.dart';
 
 class TabBottomNavigatorPage extends StatelessWidget {
   const TabBottomNavigatorPage(
-      {Key? key, required GoRouterState state, required Widget child})
+      {Key? key, required ValueKey<String> state, required this.child})
       : super(key: key);
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    final TabBottomNavigatorController tabProvider =
+    final TabBottomNavigatorController tabBottomNavigatorController =
         Provider.of<TabBottomNavigatorController>(context);
-
-    const List<BottomNavigationBarItem> bottomNavigationBarItems =
-        <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: "Home",
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: "Settings",
-      ),
-    ];
+    final GoRouter router = GoRouter.of(context);
+    final List<String> routes = ['/', "/settings"];
 
     return Scaffold(
+      body: child,
       bottomNavigationBar: BottomNavigationBar(
-        items: bottomNavigationBarItems,
-        currentIndex: tabProvider.currentIndex,
+        currentIndex: tabBottomNavigatorController.currentIndex,
         onTap: (int index) {
-          tabProvider.currentIndex = index;
+          tabBottomNavigatorController.currentIndex = index;
+          router.go(routes[index]);
         },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
       ),
     );
   }
