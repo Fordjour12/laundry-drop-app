ed43903283aaa4d20b035af37d2a5409566322a9
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-24 16:50:39 +0000

    Add authentication routes to goRouter.dart

--- lib/core/routes/goRouter.dart
+++ lib/core/routes/goRouter.dart
@@ -1,5 +1,8 @@
 // ignore: file_names
 import 'package:go_router/go_router.dart';
+import 'package:in.laundrydrop.app/app/authentication/create-account/create_account_page.dart';
+import 'package:in.laundrydrop.app/app/authentication/login/sign_in_page.dart';
+import 'package:in.laundrydrop.app/app/authentication/reset-password/reset_password_page.dart';
 import 'package:in.laundrydrop.app/app/settings/settings_page.dart';
 import 'package:in.laundrydrop.app/app/start/start_page.dart';
 import 'package:in.laundrydrop.app/app/welcome/welcome_page.dart';
@@ -8,6 +11,7 @@ class MyAppRouter {
   final GoRouter router = GoRouter(
     // routes: [
     // ShellRoute(
+    initialLocation: "/starter",
     routes: <RouteBase>[
       GoRoute(
         path: "/",
@@ -20,6 +24,27 @@ class MyAppRouter {
         path: "/starter",
         name: "starter",
         builder: (context, state) => StartPage(
+          key: state.pageKey,
+        ),
+      ),
+      GoRoute(
+        path: "/create-account",
+        name: "create-account",
+        builder: (context, state) => CreateAccountPage(
+          key: state.pageKey,
+        ),
+      ),
+      GoRoute(
+        path: '/signIn',
+        name: "signIn",
+        builder: (context, state) => SignInPage(
+          key: state.pageKey,
+        ),
+      ),
+      GoRoute(
+        path: '/reset-password',
+        name: "reset-password",
+        builder: (context, state) => ResetPasswordPage(
           key: state.pageKey,
         ),
       ),
