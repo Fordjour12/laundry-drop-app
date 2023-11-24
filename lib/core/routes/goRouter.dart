b3c814fcfc715429c3990a184e622dbe862687db
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-24 14:55:30 +0000

    Update routes in goRouter.dart

--- lib/core/routes/goRouter.dart
+++ lib/core/routes/goRouter.dart
@@ -1,34 +1,41 @@
 // ignore: file_names
 import 'package:go_router/go_router.dart';
-import 'package:in.laundrydrop.app/app/home/home_page.dart';
 import 'package:in.laundrydrop.app/app/settings/settings_page.dart';
-import 'package:in.laundrydrop.app/app/tabnavigationbar/tab_bottom_navigator_page.dart';
+import 'package:in.laundrydrop.app/app/start/start_page.dart';
+import 'package:in.laundrydrop.app/app/welcome/welcome_page.dart';
 
 class MyAppRouter {
   final GoRouter router = GoRouter(
-    routes: [
-      ShellRoute(
-        routes: <RouteBase>[
-          GoRoute(
-            path: "/",
-            name: "home",
-            builder: (context, state) => HomePage(
-              key: state.pageKey,
-            ),
-          ),
-          GoRoute(
-            path: "/settings",
-            name: "settings",
-            builder: (context, state) => SettingsPage(
-              key: state.pageKey,
-            ),
-          ),
-        ],
-        builder: (context, state, child) => TabBottomNavigatorPage(
-          state: state.pageKey,
-          child: child,
+    // routes: [
+    // ShellRoute(
+    routes: <RouteBase>[
+      GoRoute(
+        path: "/",
+        name: "welcome",
+        builder: (context, state) => WelcomePage(
+          key: state.pageKey,
+        ),
+      ),
+      GoRoute(
+        path: "/starter",
+        name: "starter",
+        builder: (context, state) => StartPage(
+          key: state.pageKey,
+        ),
+      ),
+      GoRoute(
+        path: "/settings",
+        name: "settings",
+        builder: (context, state) => SettingsPage(
+          key: state.pageKey,
         ),
       ),
     ],
+    // builder: (context, state, child) => TabBottomNavigatorPage(
+    //   state: state.pageKey,
+    //   child: child,
+    // ),
+    // ),
+    // ],
   );
 }
