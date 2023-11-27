579c93df682d9b02efb7893e8b3d859e293a4f8f
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-27 03:03:31 +0000

    Add new custom form and input widgets

---
+++ lib/core/design/widgets/custom_input.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:in.laundrydrop.app/core/constants/color_constants.dart';
+import 'package:in.laundrydrop.app/core/utils/auth_validation.dart';
+
+class CustomInput extends StatelessWidget {
+  final String? labelText;
+  final String? hintText;
+  final bool? obscureText;
+  final TextInputType? keyboardType;
+  final TextEditingController? controller;
+  final Function(String)? onChanged;
+
+// Handling the onSaved event of the TextFormField to save the field value when the form is submitted.
+// Displaying a message to the user when the form is submitted and validation fails
+  const CustomInput({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+    required this.obscureText,
+    required this.keyboardType,
+    required this.controller,
+    required this.onChanged,
+  }) : super(key: key);
+
+  static const OutlineInputBorder border = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide(
+      color: MyAppColorSwatch.secondaryColorDark,
+      width: 1,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText!,
+      keyboardType: keyboardType,
+      onChanged: onChanged,
+      enableSuggestions: true,
+      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+      validator: (string) {
+        switch (labelText) {
+          case "Email":
+            return AuthValidation.validateEmail(string);
+          case "Password":
+            return AuthValidation.validatePassword(string);
+          case "Username":
+            return AuthValidation.validateUsername(string);
+          default:
+            null;
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        border: border,
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          color: MyAppColorSwatch.secondaryColorDark,
+        ),
+        hintText: hintText,
+        enabledBorder: border,
+        focusedBorder: border,
+      ),
+    );
+  }
+}
