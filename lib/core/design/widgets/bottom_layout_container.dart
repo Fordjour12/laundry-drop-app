22af61e25fca73562483ecba3b8086a0cdf4c354
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-27 03:03:31 +0000

    Add new custom form and input widgets

---
+++ lib/core/design/widgets/bottom_layout_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:in.laundrydrop.app/core/constants/color_constants.dart';
+
+class BottomLayoutContainer extends StatelessWidget {
+  final Widget child;
+
+  // this value is used to set the height of the container  if not provided then it will take 100% of the screen height
+  // value should be in double (0.0 - 1.0   1.0 = 100%)
+  final double? height;
+
+  const BottomLayoutContainer({Key? key, required this.child, this.height})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * (height ?? 0.55),
+        decoration: const ShapeDecoration(
+          color: MyAppColorSwatch.primaryColorDark,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: child,
+      ),
+    );
+  }
+}
---
+++ lib/core/design/widgets/custom_form.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:in.laundrydrop.app/core/design/widgets/custom_button.dart';
+import 'package:in.laundrydrop.app/core/design/widgets/custom_input.dart';
+
+class CustomForm extends StatelessWidget {
+  final String buttonText;
+  final Function() onPressed;
+
+  const CustomForm({
+    Key? key,
+    this.numberOfFields = 2,
+    required this.buttonText,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final int numberOfFields;
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  static final TextEditingController _emailController = TextEditingController();
+  static final TextEditingController _passwordController =
+      TextEditingController();
+  static final TextEditingController _usernameController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomInput(
+            labelText: 'Email',
+            hintText: 'Enter your email',
+            obscureText: false,
+            keyboardType: TextInputType.emailAddress,
+            controller: _emailController,
+            onChanged: (value) {
+              _formKey.currentState!.validate();
+            },
+          ),
+          const SizedBox(height: 16),
+          CustomInput(
+            labelText: 'Password',
+            hintText: 'Enter your password',
+            obscureText: true,
+            keyboardType: TextInputType.visiblePassword,
+            controller: _passwordController,
+            onChanged: (value) {
+              _formKey.currentState!.validate();
+            },
+          ),
+          const SizedBox(height: 16),
+          if (numberOfFields == 3)
+            CustomInput(
+              labelText: 'Username',
+              hintText: 'Enter your username',
+              obscureText: false,
+              keyboardType: TextInputType.text,
+              controller: _usernameController,
+              onChanged: (value) {
+                _formKey.currentState!.validate();
+              },
+            ),
+          const SizedBox(height: 20),
+          CustomButton(
+            buttonText: buttonText,
+            onPressed: onPressed,
+          )
+        ],
+      ),
+    );
+  }
+}
