7c2dc6ca3acbf2b7b8179f0dff7352a07367211d
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-27 03:04:07 +0000

    Add AuthValidation class for email, password, and username validation

---
+++ lib/core/utils/auth_validation.dart
@@ -0,0 +1,32 @@
+class AuthValidation {
+
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    if (!value.contains('@')) {
+      return 'Email is invalid';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters';
+    }
+    return null;
+  }
+
+  static String? validateUsername(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Username is required';
+    }
+    if (value.length < 2) {
+      return 'Username must be at least 3 characters';
+    }
+    return null;
+  }
+}
