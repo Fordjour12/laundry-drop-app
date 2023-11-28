c0c1e8214af34a5e38680e5965cc8ec6ab1ec5f2
Fordjour12 <fordjourbobiem@gmail.com>
2023-11-28 13:16:26 +0000

    Add create account feature

---
+++ lib/app/authentication/create-account/create_account_repository.dart
@@ -0,0 +1,32 @@
+import 'package:in.laundrydrop.app/app/authentication/create-account/create_account.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class CreateAccountRepository {
+  final SupabaseClient supabase;
+
+  CreateAccountRepository(this.supabase);
+
+  Future<User> createAccount(CreateAccountModel userInfo) async {
+    // try {
+    final response = await supabase.auth.signUp(
+      email: userInfo.email,
+      password: userInfo.password,
+      data: {userInfo.username: userInfo.username},
+    );
+    if (response.user != null) {
+      print("User created successfully");
+      return response.user!;
+    } else {
+      throw Exception('User creation failed');
+    }
+    // } catch (error) {
+    //   print(error);
+    //   throw Exception('Error creating user: $error.message');
+    // }
+    // } on AuthException catch (error) {
+    //   print(error.statusCode);
+    // } catch (error) {
+    //   print(error);
+    // }
+  }
+}
