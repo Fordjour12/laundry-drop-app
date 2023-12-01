be15171af944e090c6b2f354c9c6d6b678d6da00
Fordjour12 <fordjourbobiem@gmail.com>
2023-12-01 11:06:42 +0000

    Refactor CreateAccountPage to use ChangeNotifierProvider

--- lib/app/authentication/create-account/create_account_page.dart
+++ lib/app/authentication/create-account/create_account_page.dart
@@ -1,90 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:in.laundrydrop.app/app/authentication/create-account/create_account_controller.dart';
 import 'package:in.laundrydrop.app/core/constants/color_constants.dart';
 import 'package:in.laundrydrop.app/core/constants/text_constants.dart';
 import 'package:in.laundrydrop.app/core/design/widgets/bottom_layout_container.dart';
 import 'package:in.laundrydrop.app/core/design/widgets/custom_form.dart';
+import 'package:in.laundrydrop.app/main.dart';
+import 'package:provider/provider.dart';
 
 class CreateAccountPage extends StatelessWidget {
   const CreateAccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Image.asset(
-              "assets/image/create_account.jpg",
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.cover,
-            ),
-            BottomLayoutContainer(
-              height: 0.69,
-              child: Column(
-                children: <Widget>[
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 16,
-                          bottom: 16,
-                        ),
-                        child: Text(
-                          "Create New Account",
-                          style: MyAppTypography.hd24b,
-                        ),
-                      ),
-                      Text(
-                        "Don’t have an account enter your details to create a new account",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          height: 21 / 14,
-                        ),
-                      ),
-                    ],
+    return ChangeNotifierProvider(
+      create: (_) => CreateAccountPageStateChange(
+        onAuthStateChanged: () => context.go('/home'),
+        supabaseClient: supabase,
+      ),
+      child: Consumer<CreateAccountPageStateChange>(
+        builder: (context, value, child) {
+          return Scaffold(
+            body: SingleChildScrollView(
+              child: Stack(
+                children: [
+                  Image.asset(
+                    "assets/image/create_account.jpg",
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    fit: BoxFit.cover,
                   ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-                  CustomForm(
-                    numberOfFields: 3,
-                    buttonText: "Create Account",
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        "Already have an account?",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          height: 21 / 14,
+                  BottomLayoutContainer(
+                    height: 0.69,
+                    child: Column(
+                      children: <Widget>[
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 16,
+                                bottom: 16,
+                              ),
+                              child: Text(
+                                "Create New Account",
+                                style: MyAppTypography.hd24b,
+                              ),
+                            ),
+                            Text(
+                              "Don’t have an account enter your details to create a new account",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                height: 21 / 14,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context.go("/sign-in");
-                        },
-                        child: const Text(
-                          "Sign In",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w700,
-                            height: 21 / 14,
-                            color: MyAppColorSwatch.tertiaryColorDarkShade1,
-                          ),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03),
+                        CustomForm(
+                          numberOfFields: 3,
+                          buttonText: "Create Account",
                         ),
-                      ),
-                    ],
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                height: 21 / 14,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                context.go("/sign-in");
+                              },
+                              child: const Text(
+                                "Sign In",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  height: 21 / 14,
+                                  color:
+                                      MyAppColorSwatch.tertiaryColorDarkShade1,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
 }
+
+
+
