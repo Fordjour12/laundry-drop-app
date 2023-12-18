7c028bf9d2e41a345acd2b60676d4f8771bfa0c9
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2023-12-18 02:06:30 +0000

    Add SignInModel class with toJson method

---
+++ lib/app/authentication/login/sign_in.dart
@@ -0,0 +1,14 @@
+class SignInModel {
+  String email;
+  String password;
+
+  SignInModel({
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'email': email,
+        'password': password,
+      };
+}
