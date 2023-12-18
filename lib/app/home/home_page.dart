122f35c69443ecfd86ae09a8f98205969b88fdff
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2023-12-18 02:06:44 +0000

    Add sign out functionality and Auth class

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:in.laundrydrop.app/core/constants/text_constants.dart';
+import 'package:in.laundrydrop.app/main.dart';
+import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,12 +11,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Text(
-          "HomePage",
-          style: MyAppTypography.body1,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "HomePage",
+                style: MyAppTypography.body1,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Sign Out");
+                supabase.auth.signOut();
+                Provider.of<Auth>(context, listen: false).setUser(null);
+              },
+              child: const Text(
+                "Sign Out",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class Auth with ChangeNotifier {
+  User? _user;
+
+  User? get user => _user;
+
+  void setUser(User? user) {
+    _user = user;
+    notifyListeners();
   }
 }
